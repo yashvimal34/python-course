d6724e9f3fd79140999f8d146d6916d72f80423a
Yash Vimal <yashsarthak79@gmail.com>
2026-03-27 21:46:11 +0530

    Day 3 - variables

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,21 @@
+# In Python, variables are used to store data that can be used to manipulated  throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=).
+
+# Python is DYNAMICALLY TYPED LANGUAGE 
+
+age = 34 # the data type of this variable is in integer.
+name = "yash" # the data type of this variable is in string.
+cgpa = 8.97 # the date type of this variable is in 'float' which means floating point number.
+
+# Rule of defining a variable in python.
+
+# 1. Variables names must start with a letter (a-z, A-Z) or an underscore (_).
+# 2. They contain letters, numbers and underscores.
+# 3. Variables name are case-sensitive ('age' and 'Age' are different).
+# 4. Avoid using python keywords (e.g., if, for, while) as variable names.
+
+# 34age = 18 # Invalid because variable cannot start with a number
+age = 32 # Valid because variable can start with a letter.
+# a$$ge = # Invalid because variables cannot contain special characters.
+__age = 34 # Valid
+__nice_age = 34 # Valid
+a_b_c_d_7 = "Sam" # Valid
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,21 @@
+# Python supports several built-in data types:
+# interger (int):
+# Floats (Float):
+# Strings (str):
+# Boolean (bool):
+
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.79
+print(cgpa)
+print(type(cgpa))
+
+name = "Yash"
+print(name)
+print(type(name))
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
