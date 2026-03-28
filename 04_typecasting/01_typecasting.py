ea84ac20c07cc1893f99d3cc06ed37dad00b6159
Yash Vimal <yashsarthak79@gmail.com>
2026-03-28 21:43:08 +0530

    04_typecasting

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,23 @@
+a = 34;
+print(a);
+print(type(a));
+
+b = "34";
+print(b);
+print(type(b))
+
+# convert 'b' into integer as it is in string let's see
+
+c = int(b);
+print(c);
+print(type(c))
+
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# convert 'pi' into an integer.
+
+pii = int(pi)
+print(pii)
+print(type(pii))
---
+++ 04_typecasting/practice_example/example1.py
@@ -0,0 +1,23 @@
+a = 890
+print(a)
+print(type(a))
+
+b = "900"
+print(b)
+print(type(b))
+
+c = int(b)
+print(c)
+print(type(c))
+
+d = float(b)
+print(d)
+print(type(d))
+
+e = str(a)
+print(e)
+print(type(e))
+
+f = True
+print(f)
+print(type(f))
