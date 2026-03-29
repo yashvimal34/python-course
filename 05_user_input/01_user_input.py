8664af55310d63ed6402b40dbfab0000f4eb704f
Yash Vimal <yashsarthak79@gmail.com>
2026-03-29 23:09:40 +0530

    05_user_input

---
+++ 05_user_input/01_user_input.py
@@ -0,0 +1,14 @@
+# a = input("Enter a number")
+# a = int(a) # convert a to int version because without int function it cannot add a string value.
+# print(a + 3)
+
+
+# a = int(input("Enter first number"))
+# b = int(input("Enter second number"))
+# print(a + b)
+
+
+
+c = float(input("Enter a first number"))
+d = float(input("Enter second number"))
+print(c>d)
---
+++ 05_user_input/practice_example/example1.py
@@ -0,0 +1,11 @@
+a = input("enter first number")
+b = (int(a))
+print(type(b))
+
+c = int(input("enter first number"))
+d = int(input("enter second number"))
+print(c + d)
+
+e = float(input("enter first number"))
+f = float(input("enter second number"))
+print(e>f)
